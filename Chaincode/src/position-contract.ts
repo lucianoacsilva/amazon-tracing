42f5a3773994c89e5a7d0afdfe353ac12db4ce73
Luciano Augusto Campagnoli da Silva <lucianos@cpqd.com.br>
2021-03-27 10:07:13 -0300

    Updates position contract

--- Chaincode/src/position-contract.ts
+++ Chaincode/src/position-contract.ts
@@ -16,13 +16,14 @@ export class PositionContract extends Contract {
     }
 
     @Transaction()
-    public async createPosition(ctx: Context, positionId: string, latitude: Number, longitude: Number, timestamp: string): Promise<void> {
+    public async createPosition(ctx: Context, positionId: string, specimenId: string, latitude: Number, longitude: Number, timestamp: string): Promise<void> {
         const exists = await this.positionExists(ctx, positionId);
         if (exists) {
             throw new Error(`The position ${positionId} already exists`);
         }
         const position = new Position();
 
+        position.specimenId = specimenId;
         position.latitude = latitude;
         position.longitude = longitude;
         position.timestamp = timestamp;
@@ -49,13 +50,19 @@ export class PositionContract extends Contract {
         if (!exists) {
             throw new Error(`The position ${positionId} does not exist`);
         }
-        const position = new Position();
 
-        position.latitude = latitude;
-        position.longitude = longitude;
-        position.timestamp = timestamp;
+        // Reads current value of position
+        const currentPosition: Position = await this.readPosition(ctx, positionId);
 
-        const buffer = Buffer.from(JSON.stringify(position));
+        // newPosition to be updated
+        const newPosition = new Position();
+
+        newPosition.specimenId = currentPosition.specimenId;
+        newPosition.latitude = latitude;
+        newPosition.longitude = longitude;
+        newPosition.timestamp = timestamp;
+
+        const buffer = Buffer.from(JSON.stringify(newPosition));
         await ctx.stub.putState(positionId, buffer);
     }
 
@@ -66,6 +73,79 @@ export class PositionContract extends Contract {
             throw new Error(`The position ${positionId} does not exist`);
         }
         await ctx.stub.deleteState(positionId);
+    }
+
+    @Transaction(false)
+    public async getHistoryOfAsset(ctx: Context, key: string): Promise<any[]> {
+        const historyIterator = await ctx.stub.getHistoryForKey(key);
+        const allResults = [];
+
+        while (true) {
+            const res = await historyIterator.next();
+
+            if (res.value && res.value.value.toString()) {
+                let Record;
+
+                try {
+                    Record = JSON.parse(res.value.value.toString('utf8'));
+                } 
+                
+                catch (err) {
+                    console.log(err);
+                    Record = res.value.value.toString('utf8');
+                }
+
+                allResults.push(Record);
+            }
+
+            if (res.done) {
+                await historyIterator.close();
+                console.info(allResults);
+                return allResults;
+            }
+        }
+        
+    }
+
+    @Transaction(false)
+    public async readAllAssets(ctx: Context): Promise<Position[]> {
+        // Query to get all assets
+        const getAllQuery = {
+            selector: {}
+        };
+
+        const iterator = await ctx.stub.getQueryResult(JSON.stringify(getAllQuery));  
+
+        const allResults = [];
+
+        // Return all assets
+
+        while (true) {
+            const res = await iterator.next();
+
+            if (res.value && res.value.value.toString()) {
+
+                const Key = res.value.key;
+                let Record;
+                try {
+                    Record = JSON.parse(res.value.value.toString('utf8'));
+                } 
+                
+                catch (err) {
+                    console.log(err);
+                    Record = res.value.value.toString('utf8');
+                }
+
+                Record.key = Key;
+                allResults.push(Record);
+            }
+
+            if (res.done) {
+                await iterator.close();
+                console.info(allResults);
+                return allResults;
+            }
+        }
     }
 
 }
