1f8923449581dd2ef489e0aa3375ca06f8f15415
Ronaldo Martins <ronaldo.nascimentomartins@gmail.com>
2021-04-20 19:16:56 -0400

    arquivos da simulação no the one

---
+++ the-one-1.6.0/the-one-1.6.0/compile.bat
@@ -0,0 +1,8 @@
+"C:\Program Files\Java\jdk1.8.0_251\bin\javac" -extdirs lib/ core/*.java
+"C:\Program Files\Java\jdk1.8.0_251\bin\javac" -extdirs lib/ movement/*.java
+"C:\Program Files\Java\jdk1.8.0_251\bin\javac" -extdirs lib/ report/*.java
+"C:\Program Files\Java\jdk1.8.0_251\bin\javac" -extdirs lib/ routing/*.java
+"C:\Program Files\Java\jdk1.8.0_251\bin\javac" -extdirs lib/ gui/*.java
+"C:\Program Files\Java\jdk1.8.0_251\bin\javac" -extdirs lib/ input/*.java
+"C:\Program Files\Java\jdk1.8.0_251\bin\javac" -extdirs lib/ applications/*.java
+"C:\Program Files\Java\jdk1.8.0_251\bin\javac" -extdirs lib/ interfaces/*.java
